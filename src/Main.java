e80e3f7591c1ecc7c1efc3f8c8773eaea26ae346
datky <tkachenko.d@cdek.ru>
2024-07-30 21:32:22 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         Printer printer = new Printer();
         Calculator calculator = new Calculator(printer);
+
+        Scanner scanner = new Scanner(System.in);
+        String stringToCalculate = scanner.nextLine();
+        String[] operands = stringToCalculate.split(" ");
+
+        String firstOperand = operands[0]; //первое число
+        String secondOperand = operands[2]; //второе число
+        String operator = operands[1]; //знак
+
+
+        calculator.
+
+
+        //35 + 56
+
+
     }
 }
