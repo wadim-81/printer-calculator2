aeb1e4e8b430980badd424de8eaa29cef52e3b6a
datky <tkachenko.d@cdek.ru>
2024-07-30 20:48:54 +0300

    Initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,8 @@
+public class Calculator {
+
+    private final Printer printer;
+
+    public Calculator(Printer printer) {
+        this.printer = printer;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Printer printer = new Printer();
+        Calculator calculator = new Calculator(printer);
+    }
+}
